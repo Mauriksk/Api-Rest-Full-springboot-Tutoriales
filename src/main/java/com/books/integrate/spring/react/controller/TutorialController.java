5fb11f01ac17112ab54cf8e29cf75db10072e1ff
Mauricio Torres <otarksk@gmail.com>
2022-02-18 20:12:09 -0300

    problemas con git resueltos

---
+++ src/main/java/com/books/integrate/spring/react/controller/TutorialController.java
@@ -0,0 +1,165 @@
+package com.books.integrate.spring.react.controller;
+
+import java.util.*;
+
+import com.books.integrate.spring.react.model.Tutorial;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.books.integrate.spring.react.repository.TutorialRepository;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api")
+public class TutorialController {
+
+	@Autowired
+	TutorialRepository tutorialRepository;
+
+	@GetMapping("/tutorials")
+	public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+		try {
+			List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+			if (title == null)
+				tutorialRepository.findAll().forEach(tutorials::add);
+			else
+				tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+			if (tutorials.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+
+			return new ResponseEntity<>(tutorials, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/tutorials/{id}")
+	public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
+		Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+		if (tutorialData.isPresent()) {
+			return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+
+	@PostMapping("/tutorials")
+	public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
+		try {
+			Tutorial _tutorial = tutorialRepository
+					.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(),  false, 500L));
+			return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+	@PutMapping("/tutorials/{id}")
+	public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+		Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+		if (tutorialData.isPresent()) {
+			Tutorial _tutorial = tutorialData.get();
+			_tutorial.setTitle(tutorial.getTitle());
+			_tutorial.setDescription(tutorial.getDescription());
+			_tutorial.setPublished(tutorial.isPublished());
+			return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+//HttpStatus
+	@DeleteMapping("/tutorials/{id}")
+	public ResponseEntity<String> deleteTutorial(@PathVariable("id") long id) {
+		try {
+			tutorialRepository.deleteById(id);
+				return new ResponseEntity<>("Tutorials DELETE!! ",HttpStatus.NO_CONTENT);
+			} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+	@DeleteMapping("/tutorials")
+	public ResponseEntity<HttpStatus> deleteAllTutorials() {
+		try {
+			tutorialRepository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+
+	}
+
+	@GetMapping("/tutorials/published")
+	public ResponseEntity<List<Tutorial>> findByPublished() {
+		try {
+			List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+
+			if (tutorials.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(tutorials, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+	@GetMapping("/tutorials/price/{id}")
+	public Long getPriceById(@PathVariable("id") Long id){
+		Optional<Tutorial> precioTutorial = tutorialRepository.findById(id);
+		if (precioTutorial.isPresent()) {
+			return precioTutorial.get().getPrice();
+		} else {
+			return 0L;
+		}
+	}
+
+
+	@DeleteMapping("/tutorials/eliminar/{title}")
+	public ResponseEntity<String> deleteByTitle(@PathVariable("title") String title) {
+		List<Tutorial> tuto = tutorialRepository.findByTitleContaining(title);
+		try {
+			tuto.forEach(tutorial -> tutorialRepository.deleteById(tutorial.getId()));
+			return new ResponseEntity<>("Eliminar tutorial ",HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+	@PutMapping("/tutorials/updateTitulo/{title}")
+	public ResponseEntity<Tutorial> updateByTitle(@PathVariable("title") String title, @RequestBody Tutorial tutorial) {
+		List<Tutorial> tituloTutorial = tutorialRepository.findByTitleContaining(title);
+		try {
+			if (tituloTutorial.size()> 0) {
+				for(Tutorial tutorials : tituloTutorial){
+					tutorials.setTitle(tutorial.getTitle());
+					tutorials.setDescription(tutorial.getDescription());
+					tutorials.setPublished(tutorial.isPublished());
+					tutorialRepository.save(tutorials);
+				}
+				return new ResponseEntity<>(HttpStatus.OK);
+			}
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} catch(Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
---
+++ src/main/java/com/books/integrate/spring/react/model/Tutorial.java
@@ -0,0 +1,75 @@
+package com.books.integrate.spring.react.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "published")
+	private boolean published;
+
+	@Column(name = "price")
+	private Long price;
+
+	public Tutorial() {
+
+	}
+
+	public Tutorial(String title, String description, boolean published, Long price) {
+		this.title = title;
+		this.description = description;
+		this.published = published;
+		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Long getPrice() {return price;}
+
+	public void setPrice(Long price) {this.price = price;}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isPublished() {
+
+		return published;
+	}
+
+	public void setPublished(boolean isPublished) {
+
+		this.published = isPublished;
+	}
+
+	@Override
+	public String toString() {
+		return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+	}
+
+}
